4445006401f67957bd58b1e8262078b1b52059bf
Bhargav Pingale <89653153+bhargav187@users.noreply.github.com>
2024-01-12 00:14:50 +0530

    Sample Commit

---
+++ app.js
@@ -0,0 +1,102 @@
+// Bike Section starts here
+function f1() {
+    document.getElementById('Img1').src = "https://www.royalenfield.com/content/dam/royal-enfield/motorcycles/himalayan/home/desktop/himalayan-home-motorcycle.jpg";
+}
+function f2() {
+    document.getElementById('Img1').src = "https://www.royalenfield.com/content/dam/royal-enfield/bullet/home/bullet-350-new.jpg";
+}
+function f3() {
+    document.getElementById('Img1').src = "https://www.royalenfield.com/content/dam/royal-enfield/motorcycles/interceptor/banner/new/desktop/interceptor_650_motorcycle.jpg";
+}
+function f4() {
+    document.getElementById('Img1').src = "https://www.royalenfield.com/content/dam/royal-enfield/motorcycles/continental-gt/banner/new/desktop/continental_gt_650_motorcycle.jpg";
+}
+function f5() {
+    document.getElementById('Img1').src = "https://www.royalenfield.com/content/dam/royal-enfield/super-meteor-650/motorcycles/home/motorcycle/desktop/super-meteor-650.jpg";
+}
+function f6() {
+    document.getElementById('Img1').src = "https://www.royalenfield.com/content/dam/royal-enfield/hunter-350/home/motorcycle/hunter-350-motorcycle-home.jpg";
+}
+function f7(){
+    document.getElementById('Img1').src = "https://www.royalenfield.com/content/dam/royal-enfield/india/motorcycles/scram-411/banners/desktop/royal-enfield-scram-411-home-motorcycle.jpg";
+}
+function f8(){
+    document.getElementById('Img1').src = "https://www.royalenfield.com/content/dam/royal-enfield/india/motorcycles/classic-350/motorcycle/classic-350-motorcycle-section.png";
+}
+function f9(){
+    document.getElementById('Img1').src = "https://www.royalenfield.com/content/dam/royal-enfield/motorcycles/meteor-350/web-banners/desktop/meteor-350-home-motorcycle.png";
+}
+function f10(){
+    document.getElementById('Img1').src = "https://www.royalenfield.com/content/dam/royal-enfield/india/motorcycles/himalayan/banner/desktop/sleethblack-banner-des.jpg";
+}
+
+// Ride Section Starts here
+
+function f11(){
+    document.getElementById('Img2').src = "https://www.royalenfield.com/content/dam/royal-enfield/india/rides/events/motoverse-2023/banner/motoverse-live-banner.png";
+}
+
+function f12(){
+    document.getElementById('Img2').src = "https://www.royalenfield.com/content/dam/royal-enfield/india/track-school-2023/banner/desktop/l1.png";
+}
+
+function f13(){
+    document.getElementById('Img2').src = "https://www.royalenfield.com/content/dam/royal-enfield/india/gt-cup-2023/banners/main-banner-2023.png";
+}
+
+function f14(){
+    document.getElementById('Img2').src = "https://www.royalenfield.com/content/dam/royal-enfield/india/home/banner/desktop/leh-rider-support.jpg";
+}
+
+function f15(){
+    document.getElementById('Img2').src = "https://www.royalenfield.com/content/dam/royal-enfield/india/home/banner/desktop/explore-guide.jpg";
+}
+
+function f16(){
+    document.getElementById('Img2').src = "https://www.royalenfield.com/content/dam/royal-enfield/india/home/rides/desktop/royal-enfield-rentals.jpg";
+}
+
+// Apparel Section Starts here
+
+function f17(){
+    document.getElementById('Img3').src = "https://www.royalenfield.com/content/dam/royal-enfield/india/home/banner/desktop/art-of-motorcycling-apparel.png";
+}
+function f18(){
+    document.getElementById('Img3').src = "https://www.royalenfield.com/content/dam/royal-enfield/super-meteor-650/motorcycles/home/apparel/apparel-home.jpg";
+}
+function f19(){
+    document.getElementById('Img3').src = "https://www.royalenfield.com/content/dam/royal-enfield/india/home/apparel/desktop/classic-scale-model.jpg";
+}
+function f20(){
+    document.getElementById('Img3').src = "https://www.royalenfield.com/content/dam/royal-enfield/india/home/apparel/desktop/royal-enfield-x-alpinestar.jpg";
+}
+function f21(){
+    document.getElementById('Img3').src = "https://www.royalenfield.com/content/dam/royal-enfield/india/home/apparel/desktop/royal-enfield-tcx.jpg";
+}
+function f22(){
+    document.getElementById('Img3').src = "https://www.royalenfield.com/content/dam/royal-enfield/india/home/banner/desktop/miy-riding-jacket.jpg";
+}
+
+// Accessories Starts here
+
+function f23(){
+    document.getElementById('Img4').src = "https://www.royalenfield.com/content/dam/royal-enfield/bullet/gma-home-new.png";
+}
+
+function f24(){
+    document.getElementById('Img4').src = "https://www.royalenfield.com/content/dam/royal-enfield/india/home/accessories/desktop/himalayan-homepage-accessories-miy.jpg";
+}
+
+// Locate us here
+
+function f25(){
+    document.getElementById('Img5').src = "https://www.royalenfield.com/content/dam/royal-enfield/india/home/locate-us/desktop/stores.jpg";
+}
+
+function f26(){
+    document.getElementById('Img5').src = "https://www.royalenfield.com/content/dam/royal-enfield/india/home/locate-us/desktop/service-centres.jpg";
+}
+
+function f27(){
+    document.getElementById('Img5').src = "https://www.royalenfield.com/content/dam/royal-enfield/india/home/locate-us/desktop/genuine-parts.jpg";
+}
